b225ca13fa258da102ceb260318d5021fa997264
esCoulthard <187909442+esCoulthard@users.noreply.github.com>
2025-01-14 14:28:31 -0700

    test

--- yt.cpp
+++ yt.cpp
@@ -15,6 +15,31 @@ string reverseString() {
     return q;
 }
 
+float calculator() {
+  cout << "Please give me two numbers" << "\n";
+  cout << "Number one:";
+  cin >> a;
+  cout << "/n";
+  cout << "Number one:";
+  cin >> b;
+  cout << "/n" << "Thank you!" << "\n" << "Now, please pick something to do with them" << "\n" << "Operation Style:";
+  cin >> q;
+  cout << "\n" << "Thank you!" << "\n";
+  if (b == 0 && q == /) {
+    cout << "I HATE YOU!!!!!!";
+    return 0;
+  }
+  else {
+    float c;
+    c = a q b
+    cout << c;
+    return 0;
+  }
+
+  
+
+}
+
 int main()
 {
   cout << reverseString() << endl;
