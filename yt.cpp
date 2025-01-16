a523a8b0b5007eeb9e92562da6dd35a29ce53982
esCoulthard <187909442+esCoulthard@users.noreply.github.com>
2025-01-16 19:24:08 +0000

    Calculator Works now :Salute:

--- yt.cpp
+++ yt.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//
-// string reverseString() {
 
-//     string x = "q";
-//     cout << "Please GIVE ME A STRING" << "\n";
-//     cin >> x;
-//     string q;
-//     for (int i = x.length()-1; i >= 0; i--) {
-//         char z = x.at(i);
-//         q += z;
-//     }
-//     return q;
-// }
+string reverseString() {
+
+    string x = "q";
+    cout << "Please GIVE ME A STRING" << "\n";
+    cin >> x;
+    string q;
+    for (int i = x.length()-1; i >= 0; i--) {
+        char z = x.at(i);
+        q += z;
+    }
+    return q;
+}
 
 float calculator() {
   int a;
